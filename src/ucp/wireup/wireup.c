ef0a8ab672ef02389f06a7e0e87598b47ecf70d8
Changcheng Liu <jerrliu@nvidia.com>
2021-09-22 15:27:56 +0800

    UCP: apply remote ECE to local QP
    Signed-off-by: Changcheng Liu <jerrliu@nvidia.com>

--- src/ucp/wireup/wireup.c
+++ src/ucp/wireup/wireup.c
@@ -398,6 +398,7 @@ ucp_wireup_connect_local(ucp_ep_h ep,
             goto out;
         }
 
+        uct_ep_set_ece(ep->uct_eps[lane], ep->remote_ece);
         status = uct_ep_connect_to_ep(ep->uct_eps[lane], dev_addr, ep_addr);
         if (status != UCS_OK) {
             goto out;
