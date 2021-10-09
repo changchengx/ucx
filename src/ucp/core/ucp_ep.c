d9150b8e66e5e40355fba8ee7d9e801fc1626069
Changcheng Liu <jerrliu@nvidia.com>
2021-10-09 22:25:47 +0800

    UCP: set remote ECE into ucp_unpacked_address::ece
    Signed-off-by: Changcheng Liu <jerrliu@nvidia.com>

--- src/ucp/core/ucp_ep.c
+++ src/ucp/core/ucp_ep.c
@@ -768,6 +768,7 @@ ucs_status_t ucp_ep_create_server_accept(ucp_worker_h worker,
         return status;
     }
 
+    remote_addr.ece = conn_request->ece;
     for (i = 0; i < remote_addr.address_count; ++i) {
         remote_addr.address_list[i].dev_addr  = conn_request->remote_dev_addr;
         remote_addr.address_list[i].dev_index = conn_request->sa_data.dev_index;
